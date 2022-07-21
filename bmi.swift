ec8b8f0dc5327c5339cc4a01ccd7dae70024625c
Burpleguy <shen.kendrew@gmail.com>
2022-07-20 22:34:30 -0400

    Create bmi.swift

---
+++ bmi.swift
@@ -0,0 +1,18 @@
+// my version of bmi.swift
+
+var weight: int = 120
+var height: int = 160
+
+bmi = weight / height * height
+
+// actual solution
+
+var height: Double = 1.85
+var weight: Double = 82
+var bmi: Double
+ 
+bmi = weight / (height * height)
+ 
+print("Your BMI is \(bmi)")
+
+// credit code academy
