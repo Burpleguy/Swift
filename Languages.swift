8226da07f445f854f9ac0e503a77e4951d6984e6
Burpleguy <shen.kendrew@gmail.com>
2022-07-21 11:05:23 -0400

    Create Languages.swift

---
+++ Languages.swift
@@ -0,0 +1,24 @@
+// Write your code below 🗽
+
+/*
+English: EN
+Spanish: ES
+Chinese: ZH
+Russian: RU
+*/ 
+
+var abbreviation: String = "AA"
+
+if abbreviation == "EN" {
+  print("english")
+} else if abbreviation == "ES" {
+  print("Spanish") 
+} else if abbreviation == "RU" {
+  print("Russian")
+} else if abbreviation == "ZH" {
+  print("Chinese")
+} else {
+  print("Abbreviation not found")
+}
+
+/* usage: replace the abbreviation strong to one of the 4 options, if it is none of them it will print an error message
